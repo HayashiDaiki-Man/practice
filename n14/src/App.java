47634a5b0fbaf3ab2c10898307393fc716189d1e
HayashiDaiki-Man <millennialz2022@gmail.com>
2022-04-23 08:32:25 +0900

    update
    ver17.0.2に脆弱性が見つかったそうなので17.0.3にアップデートした。

--- n14/src/App.java
+++ n14/src/App.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        // 10進数を2進数に変換する
+        int n;
+        String blank = "";
+
+        var sc = new Scanner(System.in);
+        try {
+            System.out.println("数値を入力してください");
+            n = sc.nextInt();
+
+            while (n > 1) {
+                if (n % 2 == 0) {
+                    System.out.print("0" + blank);
+                } else {
+                    System.out.print("1" + blank);
+                }
+                n = n / 2;
+            }
+        } finally {
+            sc.close();
+        }
     }
 }
