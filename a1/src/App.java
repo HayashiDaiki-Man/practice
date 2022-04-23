b95861c94eb4d203e4c39c68043dba781348b732
HayashiDaiki-Man <millennialz2022@gmail.com>
2022-04-23 18:12:03 +0900

    add
    Math

---
+++ a1/src/App.java
@@ -0,0 +1,16 @@
+
+public class App {
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 2;
+        int c = -12;
+        // 絶対値
+        System.out.println(Math.abs(c));
+        // 累乗
+        System.out.println((int)Math.pow(a,b));
+        // ルート
+        System.out.println(Math.sqrt(6));
+
+    }
+}
